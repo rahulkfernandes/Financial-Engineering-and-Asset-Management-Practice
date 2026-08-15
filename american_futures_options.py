8da384676335e07c3c056cd239bf76792429bb53
rahulkfernandes <rahulkfernandes@gmail.com>
2026-08-15 02:30:50 -0400

    Docs: Typehints added

--- american_futures_options.py
+++ american_futures_options.py
@@ -1,24 +1,27 @@
 import numpy as np
 
-class BinomialPricer:
-    def __init__(self, S0, K, T, n_periods, r, sigma, c):
-        """Currently works with American Options only"""
-        self.S0 = S0
-        self.K = K
-        self.T = T
-        self.n_periods = n_periods
-        self.r = r
-        self.sigma = sigma
-        self.c = c
-        
-        # Automatically precompute constants upon initialization
-        self.dt = T / n_periods
-        self.u = np.exp(sigma * np.sqrt(self.dt))
-        self.d = 1.0 / self.u
-        self.p = (np.exp((r - c) * self.dt) - self.d) / (self.u - self.d)
-        self.df = np.exp(-r * self.dt)
 
-    def price_stock_option(self, option_type='call'):
+class BinomialPricer:
+    def __init__(
+        self, S0: float, K: float, T: float, n_periods: int, r: float, sigma: float, c: float
+    ):
+        """Currently works with American Options only"""
+        self.S0: float = S0
+        self.K: float = K
+        self.T: float = T
+        self.n_periods: int = n_periods
+        self.r: float = r
+        self.sigma: float = sigma
+        self.c: float = c
+
+        # Automatically precompute constants upon initialization
+        self.dt: float = T / n_periods
+        self.u: float = np.exp(sigma * np.sqrt(self.dt))
+        self.d: float = 1.0 / self.u
+        self.p: float = (np.exp((r - c) * self.dt) - self.d) / (self.u - self.d)
+        self.df: float = np.exp(-r * self.dt)
+
+    def price_stock_option(self, option_type:str='call'):
         stock_tree = np.zeros((self.n_periods + 1, self.n_periods + 1))
         for i in range(self.n_periods + 1):
             for j in range(i + 1):
@@ -37,7 +40,7 @@ class BinomialPricer:
             for j in range(i + 1):
                 continuation = self.df * (self.p * opt_tree[i+1, j+1] + (1 - self.p) * opt_tree[i+1, j])
                 exercise = max(0, stock_tree[i, j] - self.K) if option_type == 'call' else max(0, self.K - stock_tree[i, j])
-                
+
                 opt_tree[i, j] = max(continuation, exercise)
                 if exercise > continuation + 1e-8 and exercise > 0:
                     if i < earliest_exercise_period:
@@ -51,23 +54,23 @@ class BinomialPricer:
             for j in range(i + 1):
                 s_node = self.S0 * (self.u ** j) * (self.d ** (i - j))
                 futures_tree[i, j] = s_node * np.exp((self.r - self.c) * (self.n_periods - i) * self.dt)
-                
+
         opt_tree = np.zeros((n_opt + 1, n_opt + 1))
         earliest_exercise_period = n_opt
-        
+
         for j in range(n_opt + 1):
             opt_tree[n_opt, j] = max(0, futures_tree[n_opt, j] - self.K)
-            
+
         for i in range(n_opt - 1, -1, -1):
             for j in range(i + 1):
                 continuation = self.df * (self.p * opt_tree[i+1, j+1] + (1 - self.p) * opt_tree[i+1, j])
                 exercise = max(0, futures_tree[i, j] - self.K)
-                
+
                 opt_tree[i, j] = max(continuation, exercise)
                 if exercise > continuation + 1e-8 and exercise > 0:
                     if i < earliest_exercise_period:
                         earliest_exercise_period = i
-                        
+
         return opt_tree[0, 0], earliest_exercise_period
 
 if __name__ == '__main__':
